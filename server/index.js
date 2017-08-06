a8426ca30bd1a4cb7617f20ca7bc481b07807fd7
rezwan-hossain <rezwanvoot@gmail.com>
2017-08-06 17:34:57 +0600

    some bug fix and some comment

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
-import serverRender from './render';
+// import not going to work in node index.js
+// coz import not form node. we need to use
+// babel-node  to run our server
+import serverRender from "./render";
 //app.use(express.static("../client/"));
 
 app.set("view engine", "ejs");
 
-
-
 app.get("/", (req, res) => {
-  res.render('index',  {
+  res.render("index", {
     content: serverRender()
   });
 });
