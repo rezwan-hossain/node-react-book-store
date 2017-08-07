8c7a17f8a9c9f21e81d92361d01a84bea9b0b201
rezwan-hossain <rezwanvoot@gmail.com>
2017-08-07 11:58:03 +0600

    add to heroku and push db.sql to heroku postgres database

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -1,8 +1,12 @@
 const { Pool } = require("pg");
 const pool = new Pool({
-  database: "books-dev",
-  user: "postgres",
-  password: "123456"
+  database: "dhb5p3vrjutif",
+  user: "hysiqoizkdtntj",
+  password: "08b399e15d2819efcb96675ed2d1861985696c5f0b51b042455c93d35f43f7e9",
+  host: "ec2-54-83-205-71.compute-1.amazonaws.com",
+  ssl: true
+
+  // postgres://hysiqoizkdtntj:08b399e15d2819efcb96675ed2d1861985696c5f0b51b042455c93d35f43f7e9@ec2-54-83-205-71.compute-1.amazonaws.com:5432/dhb5p3vrjutif
 });
 
 const express = require("express");
